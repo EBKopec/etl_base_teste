9c823aa5cc42bba082676a8c5e43a10d2dd19e47
EBKopec <59347351+EBKopec@users.noreply.github.com>
2020-12-28 03:11:11 -0300

    Update Functions_triggers.sql

--- Functions_triggers.sql
+++ Functions_triggers.sql
@@ -1,7 +1,7 @@
 -- create database 
 -- Caso haja necessidade de criar o banco de dados
 -- executar os comandos abaixo.
-create database nw_base_teste;
+-- create database nw_base_teste;
 
 
 --Function -> Trigger
